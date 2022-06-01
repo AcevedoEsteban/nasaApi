1f1cce0c601434f14c3e5734235f9c224b9faf7b
astevenaa <astevenaa@gmail.com>
2022-05-31 22:40:18 -0700

    clinet and server port

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import {
   Arwes,
   SoundsProvider,
@@ -14,17 +12,36 @@ import AppLayout from "./pages/AppLayout";
 import { theme, resources, sounds } from "./settings";
 
 const App = () => {
-  return <ThemeProvider theme={createTheme(theme)}>
-    <SoundsProvider sounds={createSounds(sounds)}>
-      <Arwes animate background={resources.background.large} pattern={resources.pattern}>
-        {anim => (
-          <Router>
-            <AppLayout show={anim.entered} />
-          </Router>
-        )}
-      </Arwes>
-    </SoundsProvider>
-  </ThemeProvider>;
+  return (
+    <ThemeProvider theme={createTheme(theme)}>
+      <SoundsProvider sounds={createSounds(sounds)}>
+        <Arwes
+          animate
+          background={resources.background.large}
+          pattern={resources.pattern}>
+          {(anim) => (
+            <Router>
+              <AppLayout show={anim.entered} />
+            </Router>
+          )}
+        </Arwes>
+      </SoundsProvider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
+
+var num = 4;
+function outer() {
+  var num = 2;
+  function inner() {
+    num++;
+    var num = 3;
+    console.log(num);
+    return;
+  }
+  console.log(num);
+  return;
+}
+console.log(num);
